59043a68050a0397c753b6ccd540f6b253b1edb0
testkoyetw <23208882+testkoyetw@users.noreply.github.com>
2017-12-08 01:50:10 +0800

    initial commit

--- work/planAhead/1D_Game/1D_Game.runs/impl_1/rundef.js
+++ work/planAhead/1D_Game/1D_Game.runs/impl_1/rundef.js
@@ -8,9 +8,9 @@ var WshShell = new ActiveXObject( "WScript.Shell" );
 var ProcEnv = WshShell.Environment( "Process" );
 var PathVal = ProcEnv("PATH");
 if ( PathVal.length == 0 ) {
-  PathVal = "C:/Xilinx/14.7/ISE_DS/EDK/bin/nt64;C:/Xilinx/14.7/ISE_DS/EDK/lib/nt64;C:/Xilinx/14.7/ISE_DS/ISE/bin/nt64;C:/Xilinx/14.7/ISE_DS/ISE/lib/nt64;C:/Xilinx/14.7/ISE_DS/common/bin/nt64;C:/Xilinx/14.7/ISE_DS/common/lib/nt64;C:/Xilinx/14.7/ISE_DS/PlanAhead/bin;";
+  PathVal = "E:/Xilinx/14.7/ISE_DS/EDK/bin/nt;E:/Xilinx/14.7/ISE_DS/EDK/lib/nt;E:/Xilinx/14.7/ISE_DS/ISE/bin/nt;E:/Xilinx/14.7/ISE_DS/ISE/lib/nt;E:/Xilinx/14.7/ISE_DS/common/bin/nt;E:/Xilinx/14.7/ISE_DS/common/lib/nt;E:/Xilinx/14.7/ISE_DS/PlanAhead/bin;";
 } else {
-  PathVal = "C:/Xilinx/14.7/ISE_DS/EDK/bin/nt64;C:/Xilinx/14.7/ISE_DS/EDK/lib/nt64;C:/Xilinx/14.7/ISE_DS/ISE/bin/nt64;C:/Xilinx/14.7/ISE_DS/ISE/lib/nt64;C:/Xilinx/14.7/ISE_DS/common/bin/nt64;C:/Xilinx/14.7/ISE_DS/common/lib/nt64;C:/Xilinx/14.7/ISE_DS/PlanAhead/bin;" + PathVal;
+  PathVal = "E:/Xilinx/14.7/ISE_DS/EDK/bin/nt;E:/Xilinx/14.7/ISE_DS/EDK/lib/nt;E:/Xilinx/14.7/ISE_DS/ISE/bin/nt;E:/Xilinx/14.7/ISE_DS/ISE/lib/nt;E:/Xilinx/14.7/ISE_DS/common/bin/nt;E:/Xilinx/14.7/ISE_DS/common/lib/nt;E:/Xilinx/14.7/ISE_DS/PlanAhead/bin;" + PathVal;
 }
 
 ProcEnv("PATH") = PathVal;
--- work/planAhead/1D_Game/1D_Game.runs/impl_1/runme.sh
+++ work/planAhead/1D_Game/1D_Game.runs/impl_1/runme.sh
@@ -11,9 +11,9 @@ echo "Please update the PATH and LD_LIBRARY_PATH variables below, before executi
 exit
 
 if [ -z "$PATH" ]; then
-  PATH=C:/Xilinx/14.7/ISE_DS/EDK/bin/nt64;C:/Xilinx/14.7/ISE_DS/EDK/lib/nt64;C:/Xilinx/14.7/ISE_DS/ISE/bin/nt64;C:/Xilinx/14.7/ISE_DS/ISE/lib/nt64;C:/Xilinx/14.7/ISE_DS/common/bin/nt64;C:/Xilinx/14.7/ISE_DS/common/lib/nt64:C:/Xilinx/14.7/ISE_DS/PlanAhead/bin
+  PATH=E:/Xilinx/14.7/ISE_DS/EDK/bin/nt;E:/Xilinx/14.7/ISE_DS/EDK/lib/nt;E:/Xilinx/14.7/ISE_DS/ISE/bin/nt;E:/Xilinx/14.7/ISE_DS/ISE/lib/nt;E:/Xilinx/14.7/ISE_DS/common/bin/nt;E:/Xilinx/14.7/ISE_DS/common/lib/nt:E:/Xilinx/14.7/ISE_DS/PlanAhead/bin
 else
-  PATH=C:/Xilinx/14.7/ISE_DS/EDK/bin/nt64;C:/Xilinx/14.7/ISE_DS/EDK/lib/nt64;C:/Xilinx/14.7/ISE_DS/ISE/bin/nt64;C:/Xilinx/14.7/ISE_DS/ISE/lib/nt64;C:/Xilinx/14.7/ISE_DS/common/bin/nt64;C:/Xilinx/14.7/ISE_DS/common/lib/nt64:C:/Xilinx/14.7/ISE_DS/PlanAhead/bin:$PATH
+  PATH=E:/Xilinx/14.7/ISE_DS/EDK/bin/nt;E:/Xilinx/14.7/ISE_DS/EDK/lib/nt;E:/Xilinx/14.7/ISE_DS/ISE/bin/nt;E:/Xilinx/14.7/ISE_DS/ISE/lib/nt;E:/Xilinx/14.7/ISE_DS/common/bin/nt;E:/Xilinx/14.7/ISE_DS/common/lib/nt:E:/Xilinx/14.7/ISE_DS/PlanAhead/bin:$PATH
 fi
 export PATH
 
